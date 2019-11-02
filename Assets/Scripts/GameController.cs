1f1945c4a55b7b1ebe14652e26853a07d207d718
Quiroga, Mike <MikeQuiroga@quickenloans.com>
2019-11-02 18:59:33 -0400

    Start of Project

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+//Parent controller for gameplay (need others for different gameplay modes)
 public class GameController : MonoBehaviour
 {
+    public GameBoard _GameBoard;
+
+    void Awake() {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    Loading = 0, //[Root State] Loading: Incapable of action until loading completes
+    Active = 1, //[Root State] Active: Capable of taking new action
+	InGame_Active = 1,
+	InGame_Paused = 2
+}
+
+//Responsible for Saving/Loading & Handling Major Game Events
+public class MainController : MonoBehaviour
+{
+    private GameState CurrentGameState;
+    public static Vector2 UnitResolution;
+    public static int PPU = 100;
+
+    void Awake() {
+        CurrentGameState = GameState.Loading;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentGameState = GameState.Active;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //NOTE: Realistically, this should be caught somewhere and bubble up here
+		// if(InputHub.IH.Pause.IsDown()) {
+		// 	if(CurrentGameState == GameState.InGame_Active) {
+		// 		CurrentGameState = GameState.InGame_Paused;
+		// 	} else {
+		// 		CurrentGameState = GameState.InGame_Active;
+		// 	}
+		// }
+
+        if(UnitResolution.x != Screen.currentResolution.width * PPU || UnitResolution.y != Screen.currentResolution.height * PPU)
+        {
+            SetUnitResolution();
+        }
+    }
+
+    // Set UnitResolution based on current screen size
+    void SetUnitResolution()
+    {
+        UnitResolution = new Vector2(Screen.currentResolution.width / PPU, Screen.currentResolution.height / PPU);
+    }
+}
