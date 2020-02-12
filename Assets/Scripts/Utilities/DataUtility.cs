84cd94d3d608d2df61abba38595b56e60ecc5735
Quiroga, Mike <MikeQuiroga@quickenloans.com>
2020-02-12 05:00:12 -0500

    Lots of data

--- Assets/Scripts/Utilities/DataUtility.cs
+++ Assets/Scripts/Utilities/DataUtility.cs
@@ -1,20 +1,38 @@
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System;
 
 public static class DataUtility
 {
-    public static bool CheckExistsJSON(string path)
+    //private static readonly string X = "S2FOZFJnVWtYcDJzNXY4eS9CP0UoSCtNYlFlU2hWbVk=";
+    private static readonly string X = "Q3JpcHRvZ3JhZmlhcyBjb20gUmluamRhZWwgLyBBRVM=";
+    private static readonly int KEY_SIZE = 256;
+    private static readonly int IV_SIZE = 16; // block size is 128-bit
+
+    public static bool CheckFileExists(string path)
     {
         var absPath = Application.persistentDataPath + path;
-        Debug.Log(absPath);
         return File.Exists(absPath);
+    }
+
+    public static string LoadStringFromTxt(string path)
+    {
+        var absPath = Application.persistentDataPath + path;
+
+        if(File.Exists(absPath)) {
+            return File.ReadAllText(absPath);
+        } else {
+            Debug.Log("Error! Could not load JSON from path: " + absPath);
+            return null;
+        }
     }
 
     public static T LoadFromJSON<T>(string path)
     {
         var absPath = Application.persistentDataPath + path;
-
+        Debug.Log(absPath);
         if(File.Exists(absPath)) {
             var jsonData = File.ReadAllText(absPath);
             return JsonUtility.FromJson<T>(jsonData);
@@ -28,20 +46,27 @@ public static class DataUtility
     {
         var absPath = Application.persistentDataPath + path;
         var jsonData = JsonUtility.ToJson(dataObj);
-
         File.WriteAllText(absPath, jsonData);
     }
 
     public static T LoadFromBinary<T>(string path)
     {
         var absPath = Application.persistentDataPath + path;
+        var key = Convert.FromBase64String(X);
+
         if(File.Exists(absPath)) 
         {
-            var file = new FileStream(absPath, FileMode.Open);
-            var formatter = new BinaryFormatter();
-            var loadedObj = formatter.Deserialize(file);
+            T loadedObj;
 
-            return (T)loadedObj;
+            using(var fs = new FileStream(absPath, FileMode.Open))
+            {
+                using(var cryptoStream = CreateDecryptionStream(key, fs))
+                {
+                    loadedObj = (T)ReadObjectFromStream(cryptoStream);
+                }
+            }
+
+            return loadedObj;
         } 
         else 
         {
@@ -53,10 +78,75 @@ public static class DataUtility
     public static void SaveToBinary(string path, object dataObj)
     {
         var absPath = Application.persistentDataPath + path;
-        var file = new FileStream(absPath, FileMode.OpenOrCreate);
-        var formatter = new BinaryFormatter();
+        var key = Convert.FromBase64String(X);
 
-        formatter.Serialize(file, dataObj);
-        file.Close();
+        using(var fs = new FileStream(absPath, FileMode.OpenOrCreate))
+        {
+            using(var cryptoStream = CreateEncryptionStream(key, fs))
+            {
+                WriteObjectToStream(cryptoStream, dataObj);
+            }
+            fs.Close();
+        }
+    }
+
+    //Binary Encryption: https://stackoverflow.com/questions/28791185/encrypt-net-binary-serialization-stream
+    public static void WriteObjectToStream(Stream outputStream, System.Object obj)
+    {
+        if (object.ReferenceEquals(null, obj))
+        {
+            return;
+        }
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(outputStream, obj);
+    }
+
+    public static object ReadObjectFromStream(Stream inputStream)
+    {
+        BinaryFormatter binForm = new BinaryFormatter();
+        object obj = binForm.Deserialize(inputStream);
+        return obj;
+    }
+
+    public static CryptoStream CreateEncryptionStream(byte[] key, Stream outputStream)
+    {
+        byte[] iv = new byte[IV_SIZE];
+
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            // Using a cryptographic random number generator
+            rng.GetNonZeroBytes(iv);
+        }
+
+        // Write IV to the start of the stream
+        outputStream.Write(iv, 0, iv.Length);
+
+        Rijndael rijndael = new RijndaelManaged();
+        rijndael.KeySize = KEY_SIZE;
+
+        CryptoStream encryptor = new CryptoStream(
+            outputStream,
+            rijndael.CreateEncryptor(key, iv),
+            CryptoStreamMode.Write);
+        return encryptor;
+    }
+
+    public static CryptoStream CreateDecryptionStream(byte[] key, Stream inputStream)
+    {
+        byte[] iv = new byte[IV_SIZE];
+
+        if (inputStream.Read(iv, 0, iv.Length) != iv.Length)
+        {
+            throw new ApplicationException("Failed to read IV from stream.");
+        }
+
+        Rijndael rijndael = new RijndaelManaged();
+        rijndael.KeySize = KEY_SIZE;
+
+        CryptoStream decryptor = new CryptoStream(
+            inputStream,
+            rijndael.CreateDecryptor(key, iv),
+            CryptoStreamMode.Read);
+        return decryptor;
     }
 }
--- Assets/Scripts/Utilities/GameDataManager.cs
+++ Assets/Scripts/Utilities/GameDataManager.cs
@@ -1,10 +1,13 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameDataManager : MonoBehaviour{
 
     public PlayerConfigData PlyrConfigData;
+    public PlayerGameData PlyrGameData;
 
-    //private readonly string MAIN_DATAPATH = "/TetraNights.data.json";
+    private readonly string PLAYERGAME_DATAPATH = "/TetraNights.sav";
     private readonly string PLAYERSETTING_DATAPATH = "/TetraNights.playerConfig.json";
 
     public static GameDataManager GM;
@@ -24,15 +27,24 @@ public class GameDataManager : MonoBehaviour{
     {
         if(IsPlayerDataLoaded) { return; }
         
-        if(!DataUtility.CheckExistsJSON(PLAYERSETTING_DATAPATH))
+        if(!DataUtility.CheckFileExists(PLAYERSETTING_DATAPATH))
         {
             CreateInitialPlyrCfgData();
             SavePlyrCfgData();
         }
-        else
-        {
-            PlyrConfigData = DataUtility.LoadFromJSON<PlayerConfigData>(PLAYERSETTING_DATAPATH);
+        else { 
+            PlyrConfigData = DataUtility.LoadFromJSON<PlayerConfigData>(PLAYERSETTING_DATAPATH); 
         }
+
+        if(!DataUtility.CheckFileExists(PLAYERGAME_DATAPATH))
+        {
+            CreateInitialPlyrGameData();
+            SavePlyrGameData();
+        }
+        else { 
+            PlyrGameData = DataUtility.LoadFromBinary<PlayerGameData>(PLAYERGAME_DATAPATH);
+        }
+
         IsPlayerDataLoaded = true;
     }
 
@@ -41,12 +53,31 @@ public class GameDataManager : MonoBehaviour{
         DataUtility.SaveToJSON(PLAYERSETTING_DATAPATH, PlyrConfigData);
     }
 
+    public void SavePlyrGameData()
+    {
+        DataUtility.SaveToBinary(PLAYERGAME_DATAPATH, PlyrGameData);
+    }
+
     private void CreateInitialPlyrCfgData()
     {
         PlyrConfigData = new PlayerConfigData()
         {
             MusicVolume = 0.60f,
             SoundVolume = 0.80f
+        };
+    }
+
+    private void CreateInitialPlyrGameData()
+    {
+        PlyrGameData = new PlayerGameData()
+        {
+            PlayerID = Guid.NewGuid(),
+            PlayerName = "Dreamer",
+            PlayerEmail = "",
+            BlockBrawlMaxLv = 1,
+            HighScores = new List<HighScoreRecord>(),
+            CreateDate = DateTime.UtcNow,
+            LastUpdateDate = DateTime.UtcNow
         };
     }
 }
